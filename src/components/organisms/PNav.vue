e301a26a2b01cd2efe7ef136cf16c7393cddc448
Edmund Ekott <34253790+Edmund1645@users.noreply.github.com>
2022-10-04 00:03:56 +0200

    responsive touches

--- src/components/organisms/PNav.vue
+++ src/components/organisms/PNav.vue
@@ -1,25 +1,31 @@
 <template>
   <nav class="navbar py-5">
-    <div class="container-p d-flex justify-content-between">
-      <p class="navbar-brand h1 text-bg-dark">Posty</p>
-      <div>
-        <router-link to="/" class="btn btn-link" active-class="text-light">
+    <div class="container-p d-flex justify-content-between align-items-center">
+      <p class="navbar-brand h1 text-bg-dark d-none d-md-block">Posty</p>
+      <div class="d-flex align-items-center">
+        <router-link
+          to="/"
+          class="btn btn-link btn-sm"
+          active-class="text-light">
           Feed
         </router-link>
         <router-link
           to="/explore"
-          class="btn btn-link"
+          class="btn btn-link btn-sm"
           active-class="text-light">
           Explore
         </router-link>
         <router-link
           to="/profile"
-          class="btn btn-link"
+          class="btn btn-link btn-sm"
           active-class="text-light">
           Profile
         </router-link>
       </div>
-      <p-button @click="$router.push('/post/new')" data-cy="new-post">
+      <p-button
+        @click="$router.push('/post/new')"
+        class="btn-sm"
+        data-cy="new-post">
         New Post
       </p-button>
     </div>
